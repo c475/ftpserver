8800476d8fc9e45ed8dce88f0a1012db729a004d
c475 <danjamesbond@gmail.com>
2015-06-21 19:09:27 -0700

    .

--- tests/load_test_ls.py
+++ tests/load_test_ls.py
@@ -1,99 +1,19 @@
-import socket
-import random
-import threading
+import ftplib
 import sys
-import time
+import threading
 
 
-allports = []
-
-def make_request(sock, host, port):
-    sock.connect((host, port))
-
-    # initialization message
-    d = sock.recv(1024)
-
-    sock.send("USER daniel")
-
-    # user OK
-    d = sock.recv(1024)
-
-    sock.send("PASS coolguy")
-
-    # password OK
-    d = sock.recv(1024)
-
-    sock.send("SYST")
-
-    # UNIX
-    d = sock.recv(1024)
-
-    for i in xrange(10000):
-        p1 = random.randint(144, 237)
-        p2 = random.randint(144, 237)
-        pp = (p1 * 256) + p2
-
-        while pp in allports:
-            p1 = random.randint(144, 237)
-            p2 = random.randint(144, 237)
-            pp = (p1 * 256) + p2
-
-        allports.append(pp)
-
-        data_channel = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        data_channel.bind(("127.0.0.1", pp))
-
-        sock.send("PORT 127,0,0,1," + str(p1) + "," + str(p2))
-
-        # COMMAND_OK
-        d = sock.recv(1024)
-
-        sock.send("LIST")
+def make_request(f):
+    for i in xrange(1000):
+        r.retrlines("LIST")
+    f.close()
 
 
-        # FILE_STATUS_OK
-        d = sock.recv(1024)
+ftps = []
 
-        try:
-            sock.setblocking(0)
-            # potential connection error
-            d = sock.recv(1024)
-            sock.close()
-            return None
-        except:
-            pass
-        finally:
-            sock.setblocking(1)
+for i in xrange(1000):
+    ftps.append(FTP("127.0.0.1" + str(sys.argv[1])), "Daniel" + str(i), "Password")
 
-        data_channel.listen(5)
-        newsock, addr = data_channel.accept()
-        print("accepted connection from: " + str(addr))
-
-        # receive LIST results
-        li = newsock.recv(4040)
-        print(li)
-
-        # CLOSING CONNECTION
-        d = sock.recv(1024)
-        print(d)
-
-        newsock.close()
-
-        print("CLOSED CONNECTION!!!!!!!")
-
-    sock.close()
-
-threads = []
-
-for i in xrange(1):
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-    threads.append(
-        threading.Thread(
-            target=make_request,
-            args=(s, "127.0.0.1", int(sys.argv[1]))
-        )
-    )
-
-for thread in threads:
-    thread.start()
+for f in ftps:
+    t = threading.Thread(target=make_request, args=(f,))
+    t.start()
