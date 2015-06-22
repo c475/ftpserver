9ba52b119239cb4b4c9200a57a97ef3fe187130c
c475 <danjamesbond@gmail.com>
2015-06-21 19:14:32 -0700

    .

--- tests/load_test_ls.py
+++ tests/load_test_ls.py
@@ -12,7 +12,7 @@ def make_request(f):
 ftps = []
 
 for i in xrange(1000):
-    ftps.append(ftplib.FTP(("127.0.0.1", int(sys.argv[1])), "Daniel" + str(i), "Password")
+    ftps.append(ftplib.FTP(("127.0.0.1", int(sys.argv[1])), "Daniel" + str(i), "Password"))
 
 for f in ftps:
     t = threading.Thread(target=make_request, args=(f,))
